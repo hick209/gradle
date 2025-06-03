5eda71dd2ec9bb57d1b31f5757717c22757d0968
Justin Van Dort <jvandort@gradle.com>
2025-06-03 11:53:49 -0400

    Complete ProjectMetadataController
    This is the mechanism that configuration cache uses to serialize the dependency management state of a project during
    project isolation. The existing serialization scheme was quite incomplete and did not serialize the entire set
    of project state data. This commit enhances ProjectMetadataController to serialize all component and variant state,
    thus bringing the serialized data to parity with the original data.

--- platforms/core-configuration/configuration-cache/src/test/kotlin/org/gradle/internal/cc/impl/serialization/codecs/AbstractUserTypeCodecTest.kt
+++ platforms/core-configuration/configuration-cache/src/test/kotlin/org/gradle/internal/cc/impl/serialization/codecs/AbstractUserTypeCodecTest.kt
@@ -177,6 +177,8 @@ abstract class AbstractUserTypeCodecTest {
         parameterScheme = mock(),
         actionScheme = mock(),
         attributesFactory = mock(),
+        attributeDesugaring = mock(),
+        attributeSchemaFactory = mock(),
         valueSourceProviderFactory = mock(),
         calculatedValueContainerFactory = mock(),
         patternSetFactory = mock(),
@@ -189,6 +191,5 @@ abstract class AbstractUserTypeCodecTest {
         flowProviders = mock(),
         transformStepNodeFactory = mock(),
         problems = mock(),
-        attributeDesugaring = mock(),
     )
 }
